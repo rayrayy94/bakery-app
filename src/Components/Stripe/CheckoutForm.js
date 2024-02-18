31acb1f29f0284add067abd7df21e378f7ae620c
rayrayy94 <rehanarshad1894@icloud.com>
2024-02-18 14:53:37 -0600

    update

--- src/Components/Stripe/CheckoutForm.js
+++ src/Components/Stripe/CheckoutForm.js
@@ -60,7 +60,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `http://localhost:3000/confirmPayment/${orderId}`,
+        return_url: `https://bakery-frontend-w6qy.onrender.com/confirmPayment/${orderId}`,
       },
     });
 
